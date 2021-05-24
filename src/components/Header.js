ac38573c2c49017e7cff4d60b26dc81573cbe277
Emil Persson <emilper1999@gmail.com>
2021-05-24 23:36:15 +0200

    Footer and about page added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <div className="header">
       <h1>{title}</h1>
-      <Button
-        text={showAdd ? "Close" : "Add"}
-        color={showAdd ? "red" : "green"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "red" : "green"}
+          onClick={onAdd}
+        />
+      )}
     </div>
   );
 };
