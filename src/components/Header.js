35c53053dfddb9166ede9d2e07eb3a7b1345012d
Emil Persson <emilper1999@gmail.com>
2021-05-24 21:59:28 +0200

    Add form and button action added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd }) => {
   const onClick = () => {
     console.log("click");
   };
   return (
     <div className="header">
       <h1>{title}</h1>
-      <Button title="Add" color="green" onClick={onClick} />
+      <Button title="Add" color="green" onClick={onAdd} />
     </div>
   );
 };
